d3580c85a36f891a92195bcd0f8269c070c740af
Sayandip Duary <79034615+Sayan1733@users.noreply.github.com>
2022-10-17 11:39:44 +0530

    Update Swap Numbers.cpp

--- Swap Numbers.cpp
+++ Swap Numbers.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
-#include <stdio.h>
+#include <conio.h>
 //Swap Numbers with out any third variable
 using namespace std;
+int swap(int a,int b)
+{
+	b=b+a;
+    a=b-a;
+    b=b-a;
+    cout<<"Numbers after Swap:"<<a<<","<<b;
+    return 0;
+}
 int main() {
     
     int a,b;
     
     cout<< "Enter Two Numbers:";
     cin>>a>>b;
-    b=b+a;
-    a=b-a;
-    b=b-a;
-    cout<<"Numbers after Swap:"<<a<<","<<b;
-
-    return 0;
+    swap(a,b);
+    getch();
 }
